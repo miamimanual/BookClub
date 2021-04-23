952f96d7d4097127a7f19eb042f5ea92ca5af9e5
svetlana stojanovic <svetlana.stojanovic.86@gmail.com>
2021-04-23 10:52:23 +0200

    eventList, EventForm vol.2

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -2,11 +2,27 @@ import { useState } from "react";
 
 export default function EventForm() {
     const [date, setDate] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(date);
+    };
+    /*
+    onSubmit() {
+        event.preventDefault();
+        console.log(date);
+    } */
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <label>
                 Date:
-                <input type="date" name="date" />
+                <input
+                    type="date"
+                    name="date"
+                    value={date}
+                    required
+                    onChange={(event) => setDate(event.target.value)}
+                />
             </label>
             <input type="submit" value="Submit" />
         </form>
